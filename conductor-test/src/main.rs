671d2078aef67f9d6cd44b05a22981ca28d7cf25
Andrew Minnich <aminnich3@gmail.com>
2020-08-01 23:12:30 -0400

    sine wave (again)

--- conductor-test/src/main.rs
+++ conductor-test/src/main.rs
@@ -1,27 +1,15 @@
-use conductor::{
-	manager::{AudioManager, PlaySoundSettings},
-	sound_bank::{SoundBank, SoundId},
-};
-use ggez::{
-	event::{KeyCode, KeyMods},
-	graphics, Context, GameResult,
-};
-use rand::prelude::*;
+use conductor::manager::AudioManager;
+use ggez::{graphics, Context, GameResult};
 use std::error::Error;
 
-pub struct MainState {
-	sound_id: SoundId,
+struct MainState {
 	audio_manager: AudioManager,
 }
 
 impl MainState {
 	pub fn new() -> Result<Self, Box<dyn Error>> {
-		let mut sound_bank = SoundBank::new();
-		let sound_id =
-			sound_bank.load(&std::env::current_dir().unwrap().join("assets/cymbal.ogg"))?;
 		Ok(Self {
-			sound_id,
-			audio_manager: AudioManager::new(sound_bank)?,
+			audio_manager: AudioManager::new()?,
 		})
 	}
 }
@@ -31,43 +19,21 @@ impl ggez::event::EventHandler for MainState {
 		Ok(())
 	}
 
-	fn key_down_event(
-		&mut self,
-		_ctx: &mut Context,
-		keycode: KeyCode,
-		_keymods: KeyMods,
-		_repeat: bool,
-	) {
-		match keycode {
-			KeyCode::Space => self.audio_manager.play_sound(
-				self.sound_id,
-				PlaySoundSettings {
-					pitch: rand::thread_rng().gen_range(0.5, 1.5),
-					volume: rand::thread_rng().gen_range(0.1, 1.0),
-					..Default::default()
-				},
-			),
-			_ => {}
-		}
-	}
-
 	fn draw(&mut self, ctx: &mut Context) -> GameResult {
 		graphics::clear(ctx, graphics::BLACK);
-		let text = graphics::Text::new("hi!");
-		graphics::draw(ctx, &text, graphics::DrawParam::new())?;
 		graphics::present(ctx)?;
 		Ok(())
 	}
 }
 
-fn main() -> GameResult {
+fn main() -> Result<(), Box<dyn Error>> {
 	let (mut ctx, mut event_loop) = ggez::ContextBuilder::new("conductor-test", "tesselode")
 		.modules(ggez::conf::ModuleConf {
 			audio: false,
 			..Default::default()
 		})
 		.build()?;
-	let mut main_state = MainState::new().unwrap();
+	let mut main_state = MainState::new()?;
 	ggez::event::run(&mut ctx, &mut event_loop, &mut main_state)?;
 	Ok(())
 }
