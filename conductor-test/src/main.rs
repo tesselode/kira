1e0e422021b7e2efe38b6fe7220e9a0a77215a74
Andrew Minnich <aminnich3@gmail.com>
2020-08-11 00:40:25 -0400

    undo changes to conductor-test

--- conductor-test/src/main.rs
+++ conductor-test/src/main.rs
@@ -1,31 +1,82 @@
 use conductor::{
-	manager::AudioManagerSettings,
-	metronome::MetronomeSettings,
+	instance::{InstanceId, InstanceSettings},
+	manager::{AudioManager, AudioManagerSettings},
+	metronome::{MetronomeId, MetronomeSettings},
+	project::Project,
 	sequence::{PlaySoundTaskSettings, Sequence},
+	sound::SoundId,
 	time::Time,
-	AudioManager, Project,
 };
-use std::{error::Error, path::PathBuf};
+use ggez::{
+	event::{KeyCode, KeyMods},
+	graphics, Context, GameResult,
+};
+use std::error::Error;
+
+struct MainState {
+	audio_manager: AudioManager,
+	sound_id: SoundId,
+	paused: bool,
+}
+
+impl MainState {
+	pub fn new() -> Result<Self, Box<dyn Error>> {
+		let mut project = Project::new();
+		let sound_id = project.load_sound(
+			&std::env::current_dir()
+				.unwrap()
+				.join("assets/test_loop.ogg"),
+		)?;
+		let metronome_id = project.create_metronome(128.0, MetronomeSettings::default());
+		let mut audio_manager = AudioManager::new(project, AudioManagerSettings::default())?;
+		let mut sequence = Sequence::new(metronome_id);
+		let handle = sequence.play_sound(sound_id, PlaySoundTaskSettings::default());
+		sequence.wait(Time::Beats(3.5));
+		sequence.pause_instance(handle, Some(Time::Beats(0.25)));
+		sequence.wait(Time::Beats(0.25));
+		sequence.resume_instance(handle, Some(Time::Beats(0.25)));
+		sequence.wait(Time::Beats(0.25));
+		sequence.go_to(0);
+		audio_manager.start_metronome(metronome_id).unwrap();
+		audio_manager.start_sequence(sequence).unwrap();
+		Ok(Self {
+			audio_manager,
+			sound_id,
+			paused: false,
+		})
+	}
+}
+
+impl ggez::event::EventHandler for MainState {
+	fn update(&mut self, _ctx: &mut Context) -> GameResult {
+		self.audio_manager.events();
+		Ok(())
+	}
+
+	fn key_down_event(
+		&mut self,
+		_ctx: &mut Context,
+		keycode: KeyCode,
+		_keymods: KeyMods,
+		_repeat: bool,
+	) {
+	}
+
+	fn draw(&mut self, ctx: &mut Context) -> GameResult {
+		graphics::clear(ctx, graphics::BLACK);
+		graphics::present(ctx)?;
+		Ok(())
+	}
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let mut project = Project::new();
-	let sound_id = project.load_sound(&PathBuf::from("whatever.ogg"))?;
-	let metronome_id = project.create_metronome(120.0, MetronomeSettings::default());
-	let mut audio_manager = AudioManager::new(project, AudioManagerSettings::default())?;
-	// create a new sequence that uses a previously created metronome
-	let mut sequence = Sequence::new(metronome_id);
-	// let's define the steps for the sequence:
-	// 1. wait for the next beat
-	sequence.on_interval(1.0);
-	// 2. play a sound
-	let task = sequence.play_sound(sound_id, PlaySoundTaskSettings::default());
-	// 3. wait for 4 beats
-	sequence.wait(Time::Beats(4.0));
-	// 4. stop the sound
-	sequence.stop_instance(task, None);
-	// 5. go to step 2
-	sequence.go_to(1);
-	// now that we've defined the sequence, let's start it
-	audio_manager.start_sequence(sequence)?;
+	let (mut ctx, mut event_loop) = ggez::ContextBuilder::new("conductor-test", "tesselode")
+		.modules(ggez::conf::ModuleConf {
+			audio: false,
+			..Default::default()
+		})
+		.build()?;
+	let mut main_state = MainState::new()?;
+	ggez::event::run(&mut ctx, &mut event_loop, &mut main_state)?;
 	Ok(())
 }
