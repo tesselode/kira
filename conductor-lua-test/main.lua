de3e9df2b3458211ec0bd4ed0b7746de3a4b3ba7
Andrew Minnich <aminnich3@gmail.com>
2020-10-17 22:41:00 -0400

    rewriting the lua bindings with a focus on better error handling

--- conductor-lua-test/main.lua
+++ conductor-lua-test/main.lua
@@ -1,32 +1,5 @@
 package.cpath = love.filesystem.getWorkingDirectory() .. '/target/debug/?.dll'
 
 local conductor = require 'conductor'
-
 local manager = conductor.newManager()
-local soundId = manager:loadSound('assets/loop.ogg', {
-	metadata = {
-		tempo = 128,
-		semanticDuration = {16, 'beats'},
-	},
-})
-manager:loopSound(soundId, {
-	startPoint = {8, 'beats'},
-})
-
-local callbacks = {
-	metronomeIntervalPassed = function(interval)
-		print('interval passed:', interval)
-	end,
-	custom = function(handle)
-		print('custom event:', handle)
-	end,
-}
-
-function love.update(dt)
-	manager:freeUnusedResources()
-	manager:getEvents(callbacks)
-end
-
-function love.keypressed(key)
-	if key == 'escape' then love.event.quit() end
-end
+print(manager)
--- conductor-lua/src/lib.rs
+++ conductor-lua/src/lib.rs
@@ -1,38 +1,15 @@
-mod duration;
-mod event;
-mod instance;
 mod manager;
-mod metronome;
-mod sequence;
-mod sound;
-mod tween;
 
-use event::LCustomEventHandle;
-use manager::{LAudioManager, LAudioManagerSettings};
+use manager::LAudioManager;
 use mlua::prelude::*;
 use mlua_derive::lua_module;
-use sequence::LSequence;
-
-fn new_manager(_: &Lua, settings: LAudioManagerSettings) -> LuaResult<LAudioManager> {
-	Ok(LAudioManager::new(settings).unwrap())
-}
-
-fn new_sequence(_: &Lua, _: ()) -> LuaResult<LSequence> {
-	Ok(LSequence::new())
-}
-
-fn new_custom_event_handle(_: &Lua, _: ()) -> LuaResult<LCustomEventHandle> {
-	Ok(LCustomEventHandle::new())
-}
 
 #[lua_module]
 fn conductor(lua: &Lua) -> LuaResult<LuaTable> {
-	let exports = lua.create_table()?;
-	exports.set("newManager", lua.create_function(new_manager)?)?;
-	exports.set("newSequence", lua.create_function(new_sequence)?)?;
-	exports.set(
-		"newCustomEventHandle",
-		lua.create_function(new_custom_event_handle)?,
+	let table = lua.create_table()?;
+	table.set(
+		"newManager",
+		lua.create_function(|_: &Lua, _: ()| LAudioManager::new())?,
 	)?;
-	Ok(exports)
+	Ok(table)
 }
