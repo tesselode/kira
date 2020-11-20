8bcc292d987ff27a188f9581046fa2b01935fda2
Andrew Minnich <aminnich3@gmail.com>
2020-11-20 04:30:16 -0500

    update lua bindings

--- kira-lua/src/parameter.rs
+++ kira-lua/src/parameter.rs
@@ -1,5 +1,45 @@
-use kira::parameter::ParameterId;
+use kira::parameter::{Mapping, ParameterId};
 use mlua::prelude::*;
+
+use crate::error::KiraLuaError;
+
+#[derive(Debug, Copy, Clone)]
+pub struct LMapping(pub Mapping);
+
+impl<'lua> FromLua<'lua> for LMapping {
+	fn from_lua(lua_value: LuaValue<'lua>, lua: &'lua Lua) -> LuaResult<Self> {
+		match lua_value {
+			LuaNil => Ok(LMapping(Mapping::default())),
+			LuaValue::Table(table) => {
+				let mut mapping = Mapping::default();
+				if table.contains_key("inputRange")? {
+					let input_range_table = table.get::<_, LuaTable>("inputRange")?;
+					mapping.input_range = (
+						input_range_table.get::<_, f64>(1)?,
+						input_range_table.get::<_, f64>(2)?,
+					)
+				}
+				if table.contains_key("outputRange")? {
+					let output_range_table = table.get::<_, LuaTable>("outputRange")?;
+					mapping.output_range = (
+						output_range_table.get::<_, f64>(1)?,
+						output_range_table.get::<_, f64>(2)?,
+					)
+				}
+				if table.contains_key("clampBottom")? {
+					mapping.clamp_bottom = table.get::<_, bool>("clampBottom")?;
+				}
+				if table.contains_key("clampTop")? {
+					mapping.clamp_top = table.get::<_, bool>("clampTop")?;
+				}
+				Ok(LMapping(mapping))
+			}
+			value => Err(LuaError::external(KiraLuaError::wrong_argument_type(
+				"mapping", "table", value,
+			))),
+		}
+	}
+}
 
 #[derive(Debug, Clone)]
 pub struct LParameterId(pub ParameterId);
--- kira-lua/src/value.rs
+++ kira-lua/src/value.rs
@@ -1,7 +1,10 @@
-use kira::Value;
+use kira::{parameter::Mapping, Value};
 use mlua::prelude::*;
 
-use crate::{error::KiraLuaError, parameter::LParameterId};
+use crate::{
+	error::KiraLuaError,
+	parameter::{LMapping, LParameterId},
+};
 
 pub struct LValue<T: From<f64> + Copy>(pub Value<T>);
 
@@ -9,17 +12,23 @@ impl<'lua, T: From<f64> + Copy> FromLua<'lua> for LValue<T> {
 	fn from_lua(lua_value: LuaValue<'lua>, _: &'lua Lua) -> LuaResult<Self> {
 		match &lua_value {
 			LuaValue::Number(value) => return Ok(LValue(Value::Fixed(T::from(*value)))),
+			LuaValue::Table(table) => {
+				return Ok(LValue(Value::Parameter(
+					table.get::<_, LParameterId>(1)?.0,
+					table.get::<_, LMapping>(2)?.0,
+				)))
+			}
 			LuaValue::UserData(user_data) => {
 				if user_data.is::<LParameterId>() {
 					let id = user_data.borrow::<LParameterId>()?;
-					return Ok(LValue(Value::Parameter(id.0)));
+					return Ok(LValue(Value::Parameter(id.0, Mapping::default())));
 				}
 			}
 			_ => {}
 		}
 		Err(LuaError::external(KiraLuaError::wrong_argument_type(
 			"value",
-			"number or parameter",
+			"number, parameter, or table",
 			lua_value,
 		)))
 	}
