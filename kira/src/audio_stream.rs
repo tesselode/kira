bd6904a3b07cb1d217bb9727ff7ea3c1f32776c2
Andrew Minnich <aminnich3@gmail.com>
2020-12-23 23:12:34 -0500

    revise audio stream documentation
    this changes the documentation to focus more on reasons for using AudioStreams than reasons not to, and it changes the tone to be more in line with the rest of the documentation

--- kira/src/audio_stream.rs
+++ kira/src/audio_stream.rs
@@ -1,24 +1,27 @@
-//! A low level abstraction over any audio stream.
+//! Provides an interface for sending arbitrary audio
+//! data to a mixer track.
+//!
+//! Audio streams are useful if you need to dynamically generate
+//! audio. For instance, you can use an audio stream to synthesize
+//! sound effects in real time or feed audio from a voice chat
+//! into the mixer.
+//!
+//! If you just need to play an audio file, you should probably use
+//! [instances](crate::instance).
 
 use std::fmt::Debug;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use crate::Frame;
 
-/// The AudioStream trait describes a source of [Frame](Frame).
-/// AudioStream structs are run on the audio thread, and must therefore
-/// provide samples with very low latency in order to avoid introducing delay and
-/// audio artifacts. It is recommanded you only use an AudioStream if you
-/// know you need it as they offer *virtually no features*.
-/// This is only useful if you need to have a custom audio
-/// stream within the Kira context without satisfying
-/// all the constraints that enable some of the features.
-/// An AudioStream can be started on a track
-/// via the [`AudioManager`](crate::manager::AudioManager).
+/// Produces a constant flow of audio data in real time.
 pub trait AudioStream: Debug + Send + 'static {
-	/// Called every time the mixer requires a sample to be played immediately.
-	/// It is crucial this method returns shortly as it is part of the audio thread's
-	/// main loop.
+	/// Produces the next sample.
+	///
+	/// The audio thread has to wait for this function to finish,
+	/// so it should process quickly and in a consistent amount
+	/// of time to avoid audio glitches, such as stuttering.
+	///
 	/// `dt` represents how many seconds have elapsed since the last request.
 	fn next(&mut self, dt: f64) -> Frame;
 }
