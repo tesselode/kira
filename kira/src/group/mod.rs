a0bc6e9d2cebb1ea820b28163790f35b9b6ff7fb
Andrew Minnich <aminnich3@gmail.com>
2020-12-23 23:23:27 -0500

    document the group module

--- kira/src/group/mod.rs
+++ kira/src/group/mod.rs
@@ -1,3 +1,18 @@
+//! Provides an interface for controlling multiple instances
+//! and sequences at a time.
+//!
+//! Groups can be created with [`AudioManager::add_group`](crate::manager::AudioManager::add_group).
+//! [`Sound`](crate::sound::Sound)s, [`Arrangement`](crate::arrangement::Arrangement)s
+//! and [`Sequence`](crate::sequence::Sequence)s can be assigned
+//! to any number of groups when they're created.
+//! Groups themselves can also be assigned to groups.
+//!
+//! [`pause_group`](crate::manager::AudioManager::pause_group),
+//! [`resume_group`](crate::manager::AudioManager::resume_group), and
+//! [`stop_group`](crate::manager::AudioManager::stop_group) will
+//! affect all instances that have the specified group anywhere in
+//! their ancestry.
+
 pub(crate) mod groups;
 
 use std::sync::atomic::{AtomicUsize, Ordering};
