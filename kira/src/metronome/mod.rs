44444cbb84264a25e54363780f440b320886d394
Andrew Minnich <aminnich3@gmail.com>
2020-12-29 02:22:23 -0500

    split up metronome module

--- kira/src/metronome/mod.rs
+++ kira/src/metronome/mod.rs
@@ -1,29 +1,9 @@
-use crate::{
-	command::MetronomeCommand, parameter::Parameters, tempo::Tempo, value::CachedValue, Value,
-};
+mod settings;
+
+pub use settings::MetronomeSettings;
+
+use crate::{command::MetronomeCommand, parameter::Parameters, tempo::Tempo, value::CachedValue};
 use std::vec::Drain;
-
-#[derive(Debug, Clone)]
-/// Settings for the metronome.
-pub struct MetronomeSettings {
-	/// The tempo of the metronome (in beats per minute).
-	pub tempo: Value<Tempo>,
-	/// Which intervals (in beats) the metronome should emit events for.
-	///
-	/// For example, if this is set to `vec![0.25, 0.5, 1.0]`, then
-	/// the audio manager will receive `MetronomeIntervalPassed` events
-	/// every quarter of a beat, half of a beat, and beat.
-	pub interval_events_to_emit: Vec<f64>,
-}
-
-impl Default for MetronomeSettings {
-	fn default() -> Self {
-		Self {
-			tempo: Tempo(120.0).into(),
-			interval_events_to_emit: vec![],
-		}
-	}
-}
 
 #[derive(Debug, Clone)]
 pub(crate) struct Metronome {
---
+++ kira/src/metronome/settings.rs
@@ -0,0 +1,23 @@
+use crate::{Tempo, Value};
+
+#[derive(Debug, Clone)]
+/// Settings for the metronome.
+pub struct MetronomeSettings {
+	/// The tempo of the metronome (in beats per minute).
+	pub tempo: Value<Tempo>,
+	/// Which intervals (in beats) the metronome should emit events for.
+	///
+	/// For example, if this is set to `vec![0.25, 0.5, 1.0]`, then
+	/// the audio manager will receive `MetronomeIntervalPassed` events
+	/// every quarter of a beat, half of a beat, and beat.
+	pub interval_events_to_emit: Vec<f64>,
+}
+
+impl Default for MetronomeSettings {
+	fn default() -> Self {
+		Self {
+			tempo: Tempo(120.0).into(),
+			interval_events_to_emit: vec![],
+		}
+	}
+}
