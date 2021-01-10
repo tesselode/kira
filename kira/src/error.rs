fac7ec4dbb17981f3472496a5ffc1a3223899ee9
Andrew Minnich <aminnich3@gmail.com>
2021-01-10 14:39:33 -0500

    use thiserror to reduce error boilerplate

--- kira/src/error.rs
+++ kira/src/error.rs
@@ -1,181 +1,70 @@
-use std::{error::Error, fmt::Display};
-
 use cpal::{BuildStreamError, DefaultStreamConfigError, PlayStreamError};
+use thiserror::Error;
 
 /// Something that can go wrong.
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum AudioError {
-	/// A default audio output device could not be found.
+	#[error("Cannot find the default audio output device")]
 	NoDefaultOutputDevice,
-	/// An error occurred while getting the list of supported
-	/// audio output configurations.
-	DefaultStreamConfigError(DefaultStreamConfigError),
-	/// No supported output configuration could be found.
+
+	#[error("{0}")]
+	DefaultStreamConfigError(#[from] DefaultStreamConfigError),
+
+	#[error("No supported audio configurations")]
 	NoSupportedAudioConfig,
-	/// An error occurred while setting up the audio thread.
-	BuildStreamError(BuildStreamError),
-	/// An error occurred while starting the audio thread.
-	PlayStreamError(PlayStreamError),
-	/// The queue that sends signals from the main thread
-	/// to the audio thread is full.
+
+	#[error("{0}")]
+	BuildStreamError(#[from] BuildStreamError),
+
+	#[error("{0}")]
+	PlayStreamError(#[from] PlayStreamError),
+
+	#[error("Cannot send a command to the audio thread because the queue is full")]
 	CommandQueueFull,
-	/// The backend cannot receive commands because it no
-	/// longer exists.
+
+	#[error("The backend cannot receive commands because it no longer exists")]
 	BackendDisconnected,
-	/// A channel that sends events from the audio thread
-	/// to the main thread is already being borrowed.
+
+	#[error(
+		"Cannot pop an event from a receiver because the receiver is currently mutably borrowed"
+	)]
 	EventReceiverBorrowed,
-	/// Tried to load audio that isn't mono or stereo.
+
+	#[error("Only mono and stereo audio is supported")]
 	UnsupportedChannelConfiguration,
-	/// Tried to load audio in an unsupported file format.
+
+	#[error("Only .mp3, .ogg, .flac, and .wav files are supported")]
 	UnsupportedAudioFileFormat,
-	/// Tried to add a sequence whose loop point is after
-	/// all the other steps.
+
+	#[error("The loop point of a sequence cannot be at the very end")]
 	InvalidSequenceLoopPoint,
-	/// An error occurred when interacting with the filesystem.
-	IoError(std::io::Error),
-	/// An error occurred when loading an mp3 file.
+
+	#[error("{0}")]
+	IoError(#[from] std::io::Error),
+
 	#[cfg(feature = "mp3")]
-	Mp3Error(minimp3::Error),
-	/// An mp3 file has multiple sample rates (not supported).
+	#[error("{0}")]
+	Mp3Error(#[from] minimp3::Error),
+
 	#[cfg(feature = "mp3")]
+	#[error("mp3s with variable sample rates are not supported")]
 	VariableMp3SampleRate,
-	/// The sample rate of an mp3 file could not be determined.
+
 	#[cfg(feature = "mp3")]
+	#[error("Could not get the sample rate of the mp3")]
 	UnknownMp3SampleRate,
-	/// An error occurred when loading an ogg file.
+
 	#[cfg(feature = "ogg")]
-	OggError(lewton::VorbisError),
-	/// An error occurred when loading a flac file.
+	#[error("{0}")]
+	OggError(#[from] lewton::VorbisError),
+
 	#[cfg(feature = "flac")]
-	FlacError(claxon::Error),
-	/// An error occurred when loading a wav file.
+	#[error("{0}")]
+	FlacError(#[from] claxon::Error),
+
 	#[cfg(feature = "wav")]
-	WavError(hound::Error),
-}
-
-impl Display for AudioError {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		match self {
-			AudioError::NoDefaultOutputDevice => {
-				f.write_str("Cannot find the default audio output device")
-			}
-			AudioError::DefaultStreamConfigError(error) => f.write_str(&format!("{}", error)),
-			AudioError::NoSupportedAudioConfig => f.write_str("No supported audio configurations"),
-			AudioError::BuildStreamError(error) => f.write_str(&format!("{}", error)),
-			AudioError::PlayStreamError(error) => f.write_str(&format!("{}", error)),
-			AudioError::CommandQueueFull => {
-				f.write_str("Cannot send a command to the audio thread because the queue is full")
-			}
-			AudioError::BackendDisconnected => {
-				f.write_str("The backend cannot receive commands because it no longer exists.")
-			}
-			AudioError::EventReceiverBorrowed => {
-				f.write_str("Cannot pop an event from a receiver because the receiver is currently mutably borrowed")
-			}
-			AudioError::UnsupportedChannelConfiguration => {
-				f.write_str("Only mono and stereo audio is supported")
-			}
-			AudioError::UnsupportedAudioFileFormat => {
-				f.write_str("Only .mp3, .ogg, .flac, and .wav files are supported")
-			}
-			AudioError::InvalidSequenceLoopPoint => {
-				f.write_str("The loop point of a sequence cannot be at the very end")
-			}
-			AudioError::IoError(error) => f.write_str(&format!("{}", error)),
-			#[cfg(feature = "mp3")]
-			AudioError::Mp3Error(error) => f.write_str(&format!("{}", error)),
-			#[cfg(feature = "mp3")]
-			AudioError::VariableMp3SampleRate => {
-				f.write_str("mp3s with variable sample rates are not supported")
-			}
-			#[cfg(feature = "mp3")]
-			AudioError::UnknownMp3SampleRate => f.write_str("Could not get the sample rate of the mp3"),
-			#[cfg(feature = "ogg")]
-			AudioError::OggError(error) => f.write_str(&format!("{}", error)),
-			#[cfg(feature = "flac")]
-			AudioError::FlacError(error) => f.write_str(&format!("{}", error)),
-			#[cfg(feature = "wav")]
-			AudioError::WavError(error) => f.write_str(&format!("{}", error)),
-		}
-	}
-}
-
-impl Error for AudioError {
-	fn source(&self) -> Option<&(dyn Error + 'static)> {
-		match self {
-			AudioError::NoDefaultOutputDevice => None,
-			AudioError::DefaultStreamConfigError(error) => Some(error),
-			AudioError::NoSupportedAudioConfig => None,
-			AudioError::BuildStreamError(error) => Some(error),
-			AudioError::PlayStreamError(error) => Some(error),
-			AudioError::CommandQueueFull => None,
-			AudioError::BackendDisconnected => None,
-			AudioError::EventReceiverBorrowed => None,
-			AudioError::UnsupportedChannelConfiguration => None,
-			AudioError::UnsupportedAudioFileFormat => None,
-			AudioError::InvalidSequenceLoopPoint => None,
-			AudioError::IoError(error) => Some(error),
-			AudioError::Mp3Error(error) => Some(error),
-			AudioError::VariableMp3SampleRate => None,
-			AudioError::UnknownMp3SampleRate => None,
-			AudioError::OggError(error) => Some(error),
-			AudioError::FlacError(error) => Some(error),
-			AudioError::WavError(error) => Some(error),
-		}
-	}
-}
-
-impl From<std::io::Error> for AudioError {
-	fn from(error: std::io::Error) -> Self {
-		Self::IoError(error)
-	}
-}
-
-#[cfg(feature = "mp3")]
-impl From<minimp3::Error> for AudioError {
-	fn from(error: minimp3::Error) -> Self {
-		Self::Mp3Error(error)
-	}
-}
-
-#[cfg(feature = "ogg")]
-impl From<lewton::VorbisError> for AudioError {
-	fn from(error: lewton::VorbisError) -> Self {
-		Self::OggError(error)
-	}
-}
-
-#[cfg(feature = "flac")]
-impl From<claxon::Error> for AudioError {
-	fn from(error: claxon::Error) -> Self {
-		Self::FlacError(error)
-	}
-}
-
-#[cfg(feature = "wav")]
-impl From<hound::Error> for AudioError {
-	fn from(error: hound::Error) -> Self {
-		Self::WavError(error)
-	}
-}
-
-impl From<DefaultStreamConfigError> for AudioError {
-	fn from(error: DefaultStreamConfigError) -> Self {
-		Self::DefaultStreamConfigError(error)
-	}
-}
-
-impl From<BuildStreamError> for AudioError {
-	fn from(error: BuildStreamError) -> Self {
-		Self::BuildStreamError(error)
-	}
-}
-
-impl From<PlayStreamError> for AudioError {
-	fn from(error: PlayStreamError) -> Self {
-		Self::PlayStreamError(error)
-	}
+	#[error("{0}")]
+	WavError(#[from] hound::Error),
 }
 
 /// A wrapper around the standard [`Result`](Result)
