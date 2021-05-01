c01ec71f44b5816514a8a1417fa68b7c926149bb
Andrew Minnich <aminnich3@gmail.com>
2021-05-01 13:02:40 -0400

    add a benchmark for instances with parameters

--- kira-benchmarks/benches/instances.rs
+++ kira-benchmarks/benches/instances.rs
@@ -2,7 +2,9 @@ use std::{f32::consts::PI, vec};
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use kira::{
+	instance::InstanceSettings,
 	manager::{AudioManager, AudioManagerSettings},
+	parameter::ParameterSettings,
 	sound::{Sound, SoundSettings},
 	Frame,
 };
@@ -29,7 +31,7 @@ fn create_test_sound(num_samples: usize) -> Sound {
 fn instances_benchmark(c: &mut Criterion) {
 	let mut benchmark_group = c.benchmark_group("instances");
 
-	benchmark_group.bench_function("one sound", |b| {
+	benchmark_group.bench_function("simple", |b| {
 		const NUM_INSTANCES: usize = 100_000;
 		let (mut audio_manager, mut backend) =
 			AudioManager::new_without_audio_thread(AudioManagerSettings {
@@ -43,6 +45,43 @@ fn instances_benchmark(c: &mut Criterion) {
 		// start a bunch of instances
 		for _ in 0..NUM_INSTANCES {
 			sound_handle.play(Default::default()).unwrap();
+		}
+		backend.process();
+		b.iter(|| backend.process());
+		drop(backend);
+		drop(audio_manager);
+	});
+
+	benchmark_group.bench_function("with parameters", |b| {
+		const NUM_INSTANCES: usize = 100_000;
+		let (mut audio_manager, mut backend) =
+			AudioManager::new_without_audio_thread(AudioManagerSettings {
+				num_instances: NUM_INSTANCES,
+				num_commands: NUM_INSTANCES,
+				..Default::default()
+			});
+		let parameter_1 = audio_manager
+			.add_parameter(ParameterSettings::new().value(0.5))
+			.unwrap();
+		let parameter_2 = audio_manager
+			.add_parameter(ParameterSettings::new().value(0.5))
+			.unwrap();
+		let parameter_3 = audio_manager
+			.add_parameter(ParameterSettings::new().value(0.5))
+			.unwrap();
+		// add a test sound
+		let mut sound_handle = audio_manager.add_sound(create_test_sound(48000)).unwrap();
+		backend.process();
+		// start a bunch of instances
+		for _ in 0..NUM_INSTANCES {
+			sound_handle
+				.play(
+					InstanceSettings::new()
+						.volume(&parameter_1)
+						.playback_rate(&parameter_2)
+						.panning(&parameter_3),
+				)
+				.unwrap();
 		}
 		backend.process();
 		b.iter(|| backend.process());
