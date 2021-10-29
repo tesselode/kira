601f8725eadc34eedfb96367fadf3ab2cfcd4e06
Andrew Minnich <aminnich3@gmail.com>
2021-10-28 21:41:03 -0400

    more instance tests

--- kira/tests/instance.rs
+++ kira/tests/instance.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::{error::Error, time::Duration};
 
 use kira::{
 	manager::{AudioManager, MockBackend},
+	parameter::Tween,
 	sound::{
 		instance::{InstanceSettings, InstanceState},
 		static_sound::StaticSound,
@@ -89,6 +90,52 @@ fn reports_playback_position() -> Result<(), Box<dyn Error>> {
 
 #[test]
 #[allow(clippy::float_cmp)]
+fn allows_customizing_playback_rate() -> Result<(), Box<dyn Error>> {
+	let mut manager = AudioManager::new(Default::default(), MockBackend::new(1)).unwrap();
+	let mut sound = manager.add_sound(StaticSound::from_frames(
+		1,
+		vec![Frame::from_mono(0.0); 10],
+		Default::default(),
+	))?;
+	// forward playback
+	let instance = sound.play(InstanceSettings::new().playback_rate(3.0))?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 0.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 3.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 6.0);
+	// positive playback rate, reverse on
+	let instance = sound.play(InstanceSettings::new().playback_rate(3.0).reverse(true))?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 10.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 7.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 4.0);
+	// negative playback rate, reverse off
+	let instance = sound.play(
+		InstanceSettings::new()
+			.playback_rate(-3.0)
+			.start_position(10.0),
+	)?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 10.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 7.0);
+	manager.backend_mut().process();
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 4.0);
+	Ok(())
+}
+
+#[test]
+#[allow(clippy::float_cmp)]
 fn loops_when_playing_forward() -> Result<(), Box<dyn Error>> {
 	let mut manager = AudioManager::new(Default::default(), MockBackend::new(1)).unwrap();
 	let mut sound = manager.add_sound(StaticSound::from_frames(
@@ -118,5 +165,179 @@ fn loops_when_playing_forward() -> Result<(), Box<dyn Error>> {
 	manager.backend_mut().process();
 	manager.backend_mut().on_start_processing(0.0);
 	assert_eq!(instance.position(), 8.0);
+	Ok(())
+}
+
+#[test]
+#[allow(clippy::float_cmp)]
+fn loops_when_playing_backward() -> Result<(), Box<dyn Error>> {
+	let mut manager = AudioManager::new(Default::default(), MockBackend::new(1)).unwrap();
+	let mut sound = manager.add_sound(StaticSound::from_frames(
+		1,
+		vec![Frame::from_mono(0.0); 10],
+		Default::default(),
+	))?;
+	let instance = sound.play(
+		InstanceSettings::new()
+			.loop_behavior(LoopBehavior {
+				start_position: 5.0,
+			})
+			.playback_rate(3.0)
+			.reverse(true),
+	)?;
+	manager.backend_mut().on_start_processing(0.0);
+	for _ in 0..2 {
+		manager.backend_mut().process();
+	}
+	// instance position should now be 4.0, which should loop back to 9.0
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 9.0);
+	Ok(())
+}
+
+#[test]
+fn stops_with_fade_out() -> Result<(), Box<dyn Error>> {
+	let mut manager = AudioManager::new(Default::default(), MockBackend::new(1)).unwrap();
+	let mut sound = manager.add_sound(StaticSound::from_frames(
+		1,
+		vec![Frame::from_mono(4.0); 10],
+		Default::default(),
+	))?;
+	let mut instance = sound.play(Default::default())?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(4.0).panned(0.5).panned(0.5)
+	);
+	assert_eq!(instance.state(), InstanceState::Playing);
+	instance.stop(Tween {
+		duration: Duration::from_secs(4),
+		..Default::default()
+	})?;
+	manager.backend_mut().on_start_processing(0.0);
+	for i in (1..=3).rev() {
+		assert_eq!(
+			manager.backend_mut().process(),
+			Frame::from_mono(i as f32).panned(0.5).panned(0.5)
+		);
+		assert_eq!(instance.state(), InstanceState::Stopping);
+	}
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(0.0).panned(0.5).panned(0.5)
+	);
+	assert_eq!(instance.state(), InstanceState::Stopped);
+	Ok(())
+}
+
+#[test]
+#[allow(clippy::float_cmp)]
+fn pauses_and_resumes_with_fade() -> Result<(), Box<dyn Error>> {
+	let mut manager = AudioManager::new(Default::default(), MockBackend::new(1)).unwrap();
+	let mut sound = manager.add_sound(StaticSound::from_frames(
+		1,
+		vec![Frame::from_mono(4.0); 10],
+		Default::default(),
+	))?;
+	let mut instance = sound.play(Default::default())?;
+	manager.backend_mut().on_start_processing(0.0);
+
+	// make sure playback is happening normally
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(4.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 1.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+
+	// pause the instance
+	instance.pause(Tween {
+		duration: Duration::from_secs(4),
+		..Default::default()
+	})?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(3.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 2.0);
+	assert_eq!(instance.state(), InstanceState::Pausing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(2.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 3.0);
+	assert_eq!(instance.state(), InstanceState::Pausing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(1.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 4.0);
+	assert_eq!(instance.state(), InstanceState::Pausing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(0.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 5.0);
+	assert_eq!(instance.state(), InstanceState::Paused);
+
+	// make sure the instance position doesn't change
+	for _ in 0..3 {
+		assert_eq!(
+			manager.backend_mut().process(),
+			Frame::from_mono(0.0).panned(0.5).panned(0.5)
+		);
+		manager.backend_mut().on_start_processing(0.0);
+		assert_eq!(instance.position(), 5.0);
+		assert_eq!(instance.state(), InstanceState::Paused);
+	}
+
+	// resume the instance
+	instance.resume(Tween {
+		duration: Duration::from_secs(4),
+		..Default::default()
+	})?;
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(1.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 6.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(2.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 7.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(3.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 8.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(4.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 9.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+	assert_eq!(
+		manager.backend_mut().process(),
+		Frame::from_mono(4.0).panned(0.5).panned(0.5)
+	);
+	manager.backend_mut().on_start_processing(0.0);
+	assert_eq!(instance.position(), 10.0);
+	assert_eq!(instance.state(), InstanceState::Playing);
+
 	Ok(())
 }
