bfc0355668692b64648c47841fdd60beae775772
Andrew Minnich <aminnich3@gmail.com>
2021-11-14 22:45:00 -0500

    add wav loading and streaming support

---
+++ crates/kira-wav/src/error.rs
@@ -0,0 +1,73 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum DecodeError {
+	UnsupportedBitDepth,
+	UnsupportedChannelConfiguration,
+	WavError(hound::Error),
+}
+
+impl Display for DecodeError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			DecodeError::UnsupportedBitDepth => {
+				f.write_str("Only 16 and 24-bit audio is supported")
+			}
+			DecodeError::UnsupportedChannelConfiguration => {
+				f.write_str("Only mono and stereo audio is supported")
+			}
+			DecodeError::WavError(error) => error.fmt(f),
+		}
+	}
+}
+
+impl std::error::Error for DecodeError {
+	fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+		match self {
+			DecodeError::WavError(error) => Some(error),
+			_ => None,
+		}
+	}
+}
+
+impl From<hound::Error> for DecodeError {
+	fn from(v: hound::Error) -> Self {
+		Self::WavError(v)
+	}
+}
+
+#[derive(Debug)]
+pub enum Error {
+	IoError(std::io::Error),
+	DecodeError(DecodeError),
+}
+
+impl Display for Error {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Error::IoError(error) => error.fmt(f),
+			Error::DecodeError(error) => error.fmt(f),
+		}
+	}
+}
+
+impl std::error::Error for Error {
+	fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+		match self {
+			Error::IoError(error) => Some(error),
+			Error::DecodeError(error) => Some(error),
+		}
+	}
+}
+
+impl From<std::io::Error> for Error {
+	fn from(v: std::io::Error) -> Self {
+		Self::IoError(v)
+	}
+}
+
+impl From<DecodeError> for Error {
+	fn from(v: DecodeError) -> Self {
+		Self::DecodeError(v)
+	}
+}
