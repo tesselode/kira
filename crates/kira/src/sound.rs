d3fda5106be4b1bcb1a06b5e6ca753e032d17ea9
Andrew Minnich <aminnich3@gmail.com>
2021-11-27 13:19:00 -0500

    remove tutorials from docs (they're in the book now)

--- crates/kira/src/sound.rs
+++ crates/kira/src/sound.rs
@@ -4,30 +4,6 @@ Sources of audio.
 If you just want to load sounds from audio files (mp3, ogg, etc.),
 take a look at the documentation for
 [`kira-loaders`](https://crates.io/crates/kira-loaders).
-
-## Implementing [`Sound`] and [`SoundData`]
-
-Sounds in Kira have two phases:
-1. The [`SoundData`] phase: the user has created a sound, but it is
-not yet producing sound on the audio thread. If the sound data has
-settings, they should still be customizable at this point.
-2. The [`Sound`] phase: the user has played the sound using
-[`AudioManager::play`](crate::manager::AudioManager::play), which
-transfers ownership to the audio thread.
-
-The [`SoundData`] trait has the [`into_sound`](SoundData::into_sound)
-function, which "splits" the sound data into the live [`Sound`]
-and a [`Handle`](SoundData::Handle) which the user can use to control
-the sound from gameplay code.
-
-[`Sound`]s simply produce a [`Frame`] of audio each time
-[`process`](Sound::process) is called. A [`Sound`] can be a finite
-chunk of audio, an infinite stream of audio (e.g. voice chat),
-or anything else.
-
-Kira does not provide any tools for passing messages from gameplay
-code to a [`Sound`] or vice versa. (Internally, Kira uses the
-[`ringbuf`](https://crates.io/crates/ringbuf) crate for this purpose.)
 */
 
 pub mod static_sound;
