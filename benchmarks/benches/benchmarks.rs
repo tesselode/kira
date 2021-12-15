2f16f8b4784182d2ebc618cf478dd4e56065c46a
Andrew Minnich <aminnich3@gmail.com>
2021-12-14 22:47:40 -0500

    add a benchmark that includes the on_start_processing callback

--- benchmarks/benches/benchmarks.rs
+++ benchmarks/benches/benchmarks.rs
@@ -26,6 +26,7 @@ fn create_test_sound(num_samples: usize) -> StaticSoundData {
 }
 
 fn sounds(c: &mut Criterion) {
+	// a simple test case where many sounds are being played at once
 	c.bench_function("simple", |b| {
 		const SAMPLE_RATE: u32 = 48_000;
 		const NUM_SOUNDS: usize = 100_000;
@@ -42,6 +43,34 @@ fn sounds(c: &mut Criterion) {
 		}
 		manager.backend_mut().on_start_processing();
 		b.iter(|| manager.backend_mut().process());
+	});
+
+	// similar to "simple", but also periodically calls the
+	// on_start_processing callback to measure its relative
+	// impact on the performance
+	c.bench_function("with on_start_processing callback", |b| {
+		const SAMPLE_RATE: u32 = 48_000;
+		const NUM_SOUNDS: usize = 100_000;
+		let mut manager = AudioManager::new(
+			MockBackend::new(SAMPLE_RATE),
+			AudioManagerSettings::new()
+				.sound_capacity(NUM_SOUNDS)
+				.command_capacity(NUM_SOUNDS),
+		)
+		.unwrap();
+		let sound_data = create_test_sound(SAMPLE_RATE as usize);
+		for _ in 0..NUM_SOUNDS {
+			manager.play(sound_data.clone()).unwrap();
+		}
+		manager.backend_mut().on_start_processing();
+		let mut num_iterations = 0;
+		b.iter(|| {
+			if num_iterations % 1000 == 0 {
+				manager.backend_mut().on_start_processing();
+			}
+			manager.backend_mut().process();
+			num_iterations += 1;
+		});
 	});
 }
 
