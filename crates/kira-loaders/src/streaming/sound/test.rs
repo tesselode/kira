9b50ba3777ade26835e2f118c094132042dc21cc
Andrew Minnich <aminnich3@gmail.com>
2021-12-30 10:47:36 -0500

    a few more tests
    going to hold off on creating the rest of them until i sort out playback position updating and such

--- crates/kira-loaders/src/streaming/sound/test.rs
+++ crates/kira-loaders/src/streaming/sound/test.rs
@@ -1,8 +1,9 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, time::Duration};
 
 use kira::{
 	dsp::Frame,
 	sound::{static_sound::PlaybackState, Sound},
+	tween::Tween,
 };
 
 use crate::{
@@ -81,4 +82,153 @@ fn plays_all_samples() {
 	assert_eq!(sound.process(1.0), Frame::from_mono(0.0).panned(0.5));
 	assert!(sound.finished());
 	assert_eq!(sound.state, PlaybackState::Stopped);
+}
+
+/// Tests that a `StreamingSound` will pause playback while waiting
+/// for samples from the decoder.
+#[test]
+#[allow(clippy::float_cmp)]
+fn waits_for_samples() {
+	let data = StreamingSoundData {
+		decoder: Box::new(MockDecoder::new(
+			(1..=10).map(|i| Frame::from_mono(i as f32)).collect(),
+		)),
+		settings: StreamingSoundSettings::default(),
+	};
+	let (mut sound, _, mut scheduler) = data.split().unwrap();
+
+	for _ in 0..3 {
+		assert_eq!(sound.process(1.0), Frame::ZERO.panned(0.5));
+		assert_eq!(sound.position(), 0.0);
+	}
+
+	for _ in 0..4 {
+		scheduler.run().unwrap();
+	}
+
+	for i in 1..=3 {
+		assert_eq!(sound.process(1.0), Frame::from_mono(i as f32).panned(0.5));
+		assert_eq!(sound.position(), (i - 1) as f64);
+	}
+
+	for _ in 0..3 {
+		assert_eq!(sound.process(1.0), Frame::ZERO.panned(0.5));
+		assert_eq!(sound.position(), 2.0);
+	}
+}
+
+/// Tests that a `StreamingSound` correctly reports its playback state
+/// to be queried by StreamingSoundHandle::state.
+#[test]
+fn reports_playback_state() {
+	let data = StreamingSoundData {
+		decoder: Box::new(MockDecoder::new(vec![Frame::from_mono(0.0); 2])),
+		settings: StreamingSoundSettings::new(),
+	};
+	let (mut sound, handle, mut scheduler) = data.split().unwrap();
+	while !matches!(scheduler.run().unwrap(), NextStep::End) {}
+
+	assert_eq!(handle.state(), PlaybackState::Playing);
+
+	for _ in 0..2 {
+		sound.process(1.0);
+		sound.on_start_processing();
+		assert_eq!(handle.state(), PlaybackState::Playing);
+	}
+
+	sound.process(1.0);
+	sound.on_start_processing();
+	assert_eq!(handle.state(), PlaybackState::Stopped);
+}
+
+/// Tests that a `StreamingSound` correctly reports its playback state
+/// to be queried by StreamingSoundHandle::state.
+#[test]
+#[allow(clippy::float_cmp)]
+fn reports_playback_position() {
+	let data = StreamingSoundData {
+		decoder: Box::new(MockDecoder::new(vec![Frame::from_mono(0.0); 2])),
+		settings: StreamingSoundSettings::new(),
+	};
+	let (mut sound, handle, mut scheduler) = data.split().unwrap();
+	while !matches!(scheduler.run().unwrap(), NextStep::End) {}
+
+	assert_eq!(handle.position(), 0.0);
+
+	for i in 1..=2 {
+		sound.process(1.0);
+		sound.on_start_processing();
+		assert_eq!(handle.position(), (i - 1) as f64);
+	}
+
+	sound.process(1.0);
+	sound.on_start_processing();
+	// TODO: figure out whether the final position ought to be
+	// 1.0 or 2.0
+	assert_eq!(handle.position(), 1.0);
+}
+
+/// Tests that a `StreamingSound` fades out fully before pausing
+/// and fades back in when resuming.
+#[test]
+#[allow(clippy::float_cmp)]
+fn pauses_and_resumes_with_fades() {
+	let data = StreamingSoundData {
+		decoder: Box::new(MockDecoder::new(vec![Frame::from_mono(1.0); 100])),
+		settings: StreamingSoundSettings::new(),
+	};
+	let (mut sound, mut handle, mut scheduler) = data.split().unwrap();
+	while !matches!(scheduler.run().unwrap(), NextStep::End) {}
+
+	sound.process(1.0);
+	assert_eq!(sound.position(), 0.0);
+	assert_eq!(sound.state, PlaybackState::Playing);
+
+	handle
+		.pause(Tween {
+			duration: Duration::from_secs(4),
+			..Default::default()
+		})
+		.unwrap();
+	sound.on_start_processing();
+
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.75).panned(0.5));
+	assert_eq!(sound.position(), 1.0);
+	assert_eq!(sound.state, PlaybackState::Pausing);
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.5).panned(0.5));
+	assert_eq!(sound.position(), 2.0);
+	assert_eq!(sound.state, PlaybackState::Pausing);
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.25).panned(0.5));
+	assert_eq!(sound.position(), 3.0);
+	assert_eq!(sound.state, PlaybackState::Pausing);
+
+	for _ in 0..3 {
+		assert_eq!(sound.process(1.0), Frame::from_mono(0.0).panned(0.5));
+		assert_eq!(sound.position(), 4.0);
+		assert_eq!(sound.state, PlaybackState::Paused);
+	}
+
+	handle
+		.resume(Tween {
+			duration: Duration::from_secs(4),
+			..Default::default()
+		})
+		.unwrap();
+	sound.on_start_processing();
+
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.25).panned(0.5));
+	assert_eq!(sound.position(), 5.0);
+	assert_eq!(sound.state, PlaybackState::Playing);
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.5).panned(0.5));
+	assert_eq!(sound.position(), 6.0);
+	assert_eq!(sound.state, PlaybackState::Playing);
+	assert_eq!(sound.process(1.0), Frame::from_mono(0.75).panned(0.5));
+	assert_eq!(sound.position(), 7.0);
+	assert_eq!(sound.state, PlaybackState::Playing);
+
+	for i in 0..3 {
+		assert_eq!(sound.process(1.0), Frame::from_mono(1.0).panned(0.5));
+		assert_eq!(sound.position(), i as f64 + 8.0);
+		assert_eq!(sound.state, PlaybackState::Playing);
+	}
 }
