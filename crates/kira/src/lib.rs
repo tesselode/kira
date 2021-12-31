99d41e98af4450f3a7a495ca1e9327078dbd60f3
Andrew Minnich <aminnich3@gmail.com>
2021-12-31 16:30:32 -0500

    add a warning for todos

--- crates/kira/src/lib.rs
+++ crates/kira/src/lib.rs
@@ -23,6 +23,7 @@ support for loading audio files
 #![allow(clippy::collapsible_else_if)]
 #![allow(clippy::large_enum_variant)]
 #![allow(clippy::enum_variant_names)]
+#![warn(clippy::todo)]
 #![warn(missing_docs)]
 #![allow(clippy::tabs_in_doc_comments)]
 
