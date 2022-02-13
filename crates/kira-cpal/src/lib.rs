f3be3b46103601ba7dc0274ac443bd99f9d2456e
Andrew Minnich <aminnich3@gmail.com>
2022-02-13 16:23:14 -0500

    simplify kira-cpal now that the windows thread issue is fixed
    see here: https://github.com/RustAudio/cpal/commit/03947afefb8b36ce74db49980988ef42ccc57c4d

--- crates/kira-cpal/src/lib.rs
+++ crates/kira-cpal/src/lib.rs
@@ -22,17 +22,13 @@ let mut manager = AudioManager::<CpalBackend>::new(
 #![warn(missing_docs)]
 #![allow(clippy::tabs_in_doc_comments)]
 
-use std::{
-	fmt::{Display, Formatter},
-	time::Duration,
-};
+use std::fmt::{Display, Formatter};
 
 use cpal::{
 	traits::{DeviceTrait, HostTrait, StreamTrait},
-	BuildStreamError, DefaultStreamConfigError, PlayStreamError, Stream, StreamConfig,
+	BuildStreamError, DefaultStreamConfigError, Device, PlayStreamError, Stream, StreamConfig,
 };
 use kira::manager::backend::{Backend, Renderer};
-use ringbuf::{Producer, RingBuffer};
 
 /// Errors that can occur when using the cpal backend.
 #[derive(Debug)]
@@ -91,17 +87,15 @@ impl From<PlayStreamError> for Error {
 }
 
 enum State {
-	Uninitialized {
-		config: StreamConfig,
-	},
-	Initialized {
-		stream_quit_signal_producer: Producer<()>,
-	},
+	NotStarted,
+	Started { _stream: Stream },
 }
 
 /// A backend that connects Kira to the operating system's
 /// audio APIs using [cpal](https://crates.io/crates/cpal).
 pub struct CpalBackend {
+	device: Device,
+	config: StreamConfig,
 	state: State,
 }
 
@@ -111,112 +105,47 @@ impl Backend for CpalBackend {
 	type Error = Error;
 
 	fn setup(_settings: Self::Settings) -> Result<Self, Self::Error> {
-		let config = std::thread::spawn(|| -> Result<StreamConfig, Error> {
-			let host = cpal::default_host();
-			let device = host
-				.default_output_device()
-				.ok_or(Error::NoDefaultOutputDevice)?;
-			Ok(device.default_output_config()?.config())
-		})
-		.join()
-		.unwrap()?;
+		let (device, config) = default_device_and_config()?;
 		Ok(Self {
-			state: State::Uninitialized { config },
+			device,
+			config,
+			state: State::NotStarted,
 		})
+	}
+
+	fn start(&mut self, mut renderer: Renderer) -> Result<(), Self::Error> {
+		let channels = self.config.channels;
+		let stream = self.device.build_output_stream(
+			&self.config,
+			move |data: &mut [f32], _| {
+				renderer.on_start_processing();
+				for frame in data.chunks_exact_mut(channels as usize) {
+					let out = renderer.process();
+					if channels == 1 {
+						frame[0] = (out.left + out.right) / 2.0;
+					} else {
+						frame[0] = out.left;
+						frame[1] = out.right;
+					}
+				}
+			},
+			|_| {},
+		)?;
+		stream.play()?;
+		self.state = State::Started { _stream: stream };
+		Ok(())
 	}
 
 	fn sample_rate(&self) -> u32 {
-		match &self.state {
-			State::Uninitialized { config, .. } => config.sample_rate.0,
-			State::Initialized { .. } => unreachable!(),
-		}
-	}
-
-	fn start(&mut self, renderer: Renderer) -> Result<(), Self::Error> {
-		match &mut self.state {
-			State::Uninitialized { config } => {
-				let config = config.clone();
-				let (mut setup_result_producer, mut setup_result_consumer) =
-					RingBuffer::<Result<(), Self::Error>>::new(1).split();
-				let (stream_quit_signal_producer, mut stream_quit_signal_consumer) =
-					RingBuffer::new(1).split();
-				std::thread::spawn(move || {
-					// try setting up the stream and send back the result
-					let _stream = match setup_stream(config, renderer) {
-						Ok(stream) => {
-							setup_result_producer.push(Ok(())).unwrap();
-							stream
-						}
-						Err(error) => {
-							setup_result_producer.push(Err(error)).unwrap();
-							return;
-						}
-					};
-					// sleep until the thread receives the quit signal
-					loop {
-						std::thread::sleep(Duration::from_millis(100));
-						if stream_quit_signal_consumer.pop().is_some() {
-							break;
-						}
-					}
-				});
-				// wait for a result to come back
-				loop {
-					if let Some(result) = setup_result_consumer.pop() {
-						break result;
-					}
-					std::thread::sleep(Duration::from_millis(10));
-				}?;
-				self.state = State::Initialized {
-					stream_quit_signal_producer,
-				};
-				Ok(())
-			}
-			State::Initialized { .. } => panic!("Cannot initialize an already-initialized backend"),
-		}
+		self.config.sample_rate.0
 	}
 }
 
-impl Drop for CpalBackend {
-	fn drop(&mut self) {
-		if let State::Initialized {
-			stream_quit_signal_producer,
-		} = &mut self.state
-		{
-			if stream_quit_signal_producer.push(()).is_err() {
-				panic!("Could not send the quit signal to end the audio thread");
-			}
-		}
-	}
-}
-
-fn setup_stream(config: StreamConfig, mut renderer: Renderer) -> Result<Stream, Error> {
-	let device = cpal::default_host()
+fn default_device_and_config() -> Result<(Device, StreamConfig), Error> {
+	let host = cpal::default_host();
+	let device = host
 		.default_output_device()
 		.ok_or(Error::NoDefaultOutputDevice)?;
-	let channels = config.channels;
-	let stream = device.build_output_stream(
-		&config,
-		move |data: &mut [f32], _| {
-			#[cfg(feature = "assert_no_alloc")]
-			assert_no_alloc::assert_no_alloc(|| renderer.on_start_processing());
-			#[cfg(not(feature = "assert_no_alloc"))]
-			renderer.on_start_processing();
-			for frame in data.chunks_exact_mut(channels as usize) {
-				#[cfg(feature = "assert_no_alloc")]
-				let out = assert_no_alloc::assert_no_alloc(|| renderer.process());
-				#[cfg(not(feature = "assert_no_alloc"))]
-				let out = renderer.process();
-				if channels == 1 {
-					frame[0] = (out.left + out.right) / 2.0;
-				} else {
-					frame[0] = out.left;
-					frame[1] = out.right;
-				}
-			}
-		},
-		move |_| {},
-	)?;
-	stream.play()?;
-	Ok(stream)
+	let config = device.default_output_config()?.config();
+	Ok((device, config))
 }
