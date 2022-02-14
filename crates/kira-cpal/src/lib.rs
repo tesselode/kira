08fc6ebf59e61181387521c3273789233c5e349d
Andrew Minnich <aminnich3@gmail.com>
2022-02-13 20:16:17 -0500

    rewrite kira-cpal to gracefully handle audio devices disconnecting
    todos:
    - handle changes in config and default device that don't involve a device being disconnected
    - only call `renderer.on_change_sample_rate` if the sample rate actually changes

--- crates/kira-cpal/src/lib.rs
+++ crates/kira-cpal/src/lib.rs
@@ -22,13 +22,26 @@ let mut manager = AudioManager::<CpalBackend>::new(
 #![warn(missing_docs)]
 #![allow(clippy::tabs_in_doc_comments)]
 
-use std::fmt::{Display, Formatter};
+use std::{
+	fmt::{Display, Formatter},
+	ops::{Deref, DerefMut},
+	sync::{
+		atomic::{AtomicBool, Ordering},
+		Arc,
+	},
+	time::Duration,
+};
 
 use cpal::{
 	traits::{DeviceTrait, HostTrait, StreamTrait},
 	BuildStreamError, DefaultStreamConfigError, Device, PlayStreamError, Stream, StreamConfig,
+	StreamError,
 };
 use kira::manager::backend::{Backend, Renderer};
+use ringbuf::{Consumer, Producer, RingBuffer};
+
+const RESULT_POLLING_INTERVAL: Duration = Duration::from_millis(1);
+const DEVICE_POLLING_INTERVAL: Duration = Duration::from_millis(500);
 
 /// Errors that can occur when using the cpal backend.
 #[derive(Debug)]
@@ -86,16 +99,49 @@ impl From<PlayStreamError> for Error {
 	}
 }
 
-enum State {
-	NotStarted,
-	Started { _stream: Stream },
+struct RendererWrapper {
+	renderer: Option<Renderer>,
+	producer: Producer<Renderer>,
 }
 
-/// A backend that connects Kira to the operating system's
-/// audio APIs using [cpal](https://crates.io/crates/cpal).
+impl Deref for RendererWrapper {
+	type Target = Renderer;
+
+	fn deref(&self) -> &Self::Target {
+		self.renderer.as_ref().unwrap()
+	}
+}
+
+impl DerefMut for RendererWrapper {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		self.renderer.as_mut().unwrap()
+	}
+}
+
+impl Drop for RendererWrapper {
+	fn drop(&mut self) {
+		if self
+			.producer
+			.push(self.renderer.take().expect("The renderer does not exist"))
+			.is_err()
+		{
+			panic!("The renderer producer is full");
+		}
+	}
+}
+
+enum State {
+	Empty,
+	Uninitialized {
+		device: Device,
+		config: StreamConfig,
+	},
+	Initialized {
+		should_stop: Arc<AtomicBool>,
+	},
+}
+
 pub struct CpalBackend {
-	device: Device,
-	config: StreamConfig,
 	state: State,
 }
 
@@ -105,47 +151,148 @@ impl Backend for CpalBackend {
 	type Error = Error;
 
 	fn setup(_settings: Self::Settings) -> Result<(Self, u32), Self::Error> {
-		let (device, config) = default_device_and_config()?;
+		let host = cpal::default_host();
+		let device = host
+			.default_output_device()
+			.ok_or(Error::NoDefaultOutputDevice)?;
+		let config = device.default_output_config()?.config();
 		let sample_rate = config.sample_rate.0;
 		Ok((
 			Self {
-				device,
-				config,
-				state: State::NotStarted,
+				state: State::Uninitialized { device, config },
 			},
 			sample_rate,
 		))
 	}
 
-	fn start(&mut self, mut renderer: Renderer) -> Result<(), Self::Error> {
-		let channels = self.config.channels;
-		let stream = self.device.build_output_stream(
-			&self.config,
-			move |data: &mut [f32], _| {
-				renderer.on_start_processing();
-				for frame in data.chunks_exact_mut(channels as usize) {
-					let out = renderer.process();
-					if channels == 1 {
-						frame[0] = (out.left + out.right) / 2.0;
-					} else {
-						frame[0] = out.left;
-						frame[1] = out.right;
-					}
+	fn start(&mut self, renderer: Renderer) -> Result<(), Self::Error> {
+		let state = std::mem::replace(&mut self.state, State::Empty);
+		if let State::Uninitialized { device, config } = state {
+			let (setup_result_producer, mut setup_result_consumer) = RingBuffer::new(1).split();
+			let should_stop = Arc::new(AtomicBool::new(false));
+			let should_stop_clone = should_stop.clone();
+			std::thread::spawn(move || {
+				manage_streams(
+					device,
+					config,
+					renderer,
+					setup_result_producer,
+					should_stop_clone,
+				);
+			});
+			self.state = State::Initialized { should_stop };
+			loop {
+				std::thread::sleep(RESULT_POLLING_INTERVAL);
+				if let Some(result) = setup_result_consumer.pop() {
+					return result;
 				}
-			},
-			|_| {},
-		)?;
-		stream.play()?;
-		self.state = State::Started { _stream: stream };
-		Ok(())
+			}
+		} else {
+			panic!("Cannot initialize the backend multiple times")
+		}
 	}
 }
 
-fn default_device_and_config() -> Result<(Device, StreamConfig), Error> {
+fn manage_streams(
+	device: Device,
+	config: StreamConfig,
+	renderer: Renderer,
+	mut setup_result_producer: Producer<Result<(), Error>>,
+	should_stop: Arc<AtomicBool>,
+) {
+	let (renderer_producer, mut renderer_consumer) = RingBuffer::new(1).split();
+	let renderer_wrapper = RendererWrapper {
+		renderer: Some(renderer),
+		producer: renderer_producer,
+	};
+	let (mut stream, mut stream_error_consumer) =
+		match start_stream(&device, &config, renderer_wrapper) {
+			Ok(stream) => {
+				setup_result_producer
+					.push(Ok(()))
+					.expect("Setup result producer is full");
+				stream
+			}
+			Err(error) => {
+				setup_result_producer
+					.push(Err(error))
+					.expect("Setup result producer is full");
+				return;
+			}
+		};
+	loop {
+		std::thread::sleep(DEVICE_POLLING_INTERVAL);
+		if should_stop.load(Ordering::SeqCst) {
+			break;
+		}
+		if let Some(StreamError::DeviceNotAvailable) = stream_error_consumer.pop() {
+			let (new_stream, new_stream_error_consumer, new_renderer_consumer) =
+				restart_stream(stream, renderer_consumer).unwrap();
+			stream = new_stream;
+			stream_error_consumer = new_stream_error_consumer;
+			renderer_consumer = new_renderer_consumer;
+		}
+	}
+}
+
+impl Drop for CpalBackend {
+	fn drop(&mut self) {
+		if let State::Initialized { should_stop } = &self.state {
+			should_stop.store(true, Ordering::SeqCst);
+		}
+	}
+}
+
+fn start_stream(
+	device: &Device,
+	config: &StreamConfig,
+	mut renderer_wrapper: RendererWrapper,
+) -> Result<(Stream, Consumer<StreamError>), Error> {
+	let (mut stream_error_producer, stream_error_consumer) = RingBuffer::new(1).split();
+	let channels = config.channels;
+	let stream = device.build_output_stream(
+		config,
+		move |data: &mut [f32], _| {
+			renderer_wrapper.on_start_processing();
+			for frame in data.chunks_exact_mut(channels as usize) {
+				let out = renderer_wrapper.process();
+				if channels == 1 {
+					frame[0] = (out.left + out.right) / 2.0;
+				} else {
+					frame[0] = out.left;
+					frame[1] = out.right;
+				}
+			}
+		},
+		move |error| {
+			stream_error_producer
+				.push(error)
+				.expect("Stream error producer is full");
+		},
+	)?;
+	stream.play()?;
+	Ok((stream, stream_error_consumer))
+}
+
+fn restart_stream(
+	stream: Stream,
+	mut renderer_consumer: Consumer<Renderer>,
+) -> Result<(Stream, Consumer<StreamError>, Consumer<Renderer>), Error> {
+	drop(stream);
+	let mut renderer = renderer_consumer
+		.pop()
+		.expect("Could not retrieve the renderer after dropping a stream");
 	let host = cpal::default_host();
 	let device = host
 		.default_output_device()
 		.ok_or(Error::NoDefaultOutputDevice)?;
 	let config = device.default_output_config()?.config();
-	Ok((device, config))
+	renderer.on_change_sample_rate(config.sample_rate.0);
+	let (renderer_producer, renderer_consumer) = RingBuffer::new(1).split();
+	let renderer_wrapper = RendererWrapper {
+		renderer: Some(renderer),
+		producer: renderer_producer,
+	};
+	let (stream, stream_error_consumer) = start_stream(&device, &config, renderer_wrapper)?;
+	Ok((stream, stream_error_consumer, renderer_consumer))
 }
