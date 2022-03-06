5c869a1aed195aca968e0fe9da849d04f8188411
Andrew Minnich <aminnich3@gmail.com>
2022-03-06 13:18:50 -0500

    allow missing docs for now

--- crates/kira/src/lib.rs
+++ crates/kira/src/lib.rs
@@ -17,7 +17,7 @@ on how to use Kira.
 #![allow(clippy::large_enum_variant)]
 #![allow(clippy::enum_variant_names)]
 #![warn(clippy::todo)]
-#![warn(missing_docs)]
+// #![warn(missing_docs)]
 #![allow(clippy::tabs_in_doc_comments)]
 
 pub mod clock;
