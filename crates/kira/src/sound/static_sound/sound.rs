35fe5b815abb62403a42a71584df91a253484593
Andrew Minnich <aminnich3@gmail.com>
2022-08-20 01:04:30 -0400

    StaticSound spring cleaning

--- crates/kira/src/sound/static_sound/sound.rs
+++ crates/kira/src/sound/static_sound/sound.rs
@@ -1,5 +1,8 @@
 mod resampler;
 
+#[cfg(test)]
+mod test;
+
 use std::sync::{
 	atomic::{AtomicU64, AtomicU8, Ordering},
 	Arc,
@@ -18,49 +21,7 @@ use crate::{
 
 use self::resampler::Resampler;
 
-use super::{data::StaticSoundData, Command};
-
-#[cfg(test)]
-mod test;
-
-/// The playback state of a sound.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum PlaybackState {
-	/// The sound is playing normally.
-	Playing,
-	/// The sound is fading out, and when the fade-out
-	/// is finished, playback will pause.
-	Pausing,
-	/// Playback is paused.
-	Paused,
-	/// The sound is fading out, and when the fade-out
-	/// is finished, playback will stop.
-	Stopping,
-	/// The sound has stopped and can no longer be resumed.
-	Stopped,
-}
-
-pub(super) struct Shared {
-	state: AtomicU8,
-	position: AtomicU64,
-}
-
-impl Shared {
-	pub fn state(&self) -> PlaybackState {
-		match self.state.load(Ordering::SeqCst) {
-			0 => PlaybackState::Playing,
-			1 => PlaybackState::Pausing,
-			2 => PlaybackState::Paused,
-			3 => PlaybackState::Stopping,
-			4 => PlaybackState::Stopped,
-			_ => panic!("Invalid playback state"),
-		}
-	}
-
-	pub fn position(&self) -> f64 {
-		f64::from_bits(self.position.load(Ordering::SeqCst))
-	}
-}
+use super::{data::StaticSoundData, Command, StaticSoundSettings};
 
 pub(super) struct StaticSound {
 	command_consumer: Consumer<Command>,
@@ -68,7 +29,7 @@ pub(super) struct StaticSound {
 	start_time: StartTime,
 	state: PlaybackState,
 	resampler: Resampler,
-	current_sample_index: usize,
+	current_frame_index: usize,
 	fractional_position: f64,
 	volume: Tweener<Volume>,
 	playback_rate: Tweener<PlaybackRate>,
@@ -80,31 +41,20 @@ pub(super) struct StaticSound {
 impl StaticSound {
 	pub fn new(data: StaticSoundData, command_consumer: Consumer<Command>) -> Self {
 		let settings = data.settings;
-		let current_sample_index = if settings.reverse {
-			let position_seconds = data.duration().as_secs_f64() - settings.start_position;
-			(position_seconds * data.sample_rate as f64) as usize - 1
-		} else {
-			(settings.start_position * data.sample_rate as f64) as usize
-		};
-		let position = current_sample_index as f64 / data.sample_rate as f64;
+		let starting_frame_index = starting_frame_index(settings, &data);
+		let position = starting_frame_index as f64 / data.sample_rate as f64;
 		let mut sound = Self {
 			command_consumer,
 			data,
 			start_time: settings.start_time,
 			state: PlaybackState::Playing,
-			resampler: Resampler::new(current_sample_index),
-			current_sample_index,
+			resampler: Resampler::new(starting_frame_index),
+			current_frame_index: starting_frame_index,
 			fractional_position: 0.0,
 			volume: Tweener::new(settings.volume),
 			playback_rate: Tweener::new(settings.playback_rate),
 			panning: Tweener::new(settings.panning),
-			volume_fade: if let Some(tween) = settings.fade_in_tween {
-				let mut tweenable = Tweener::new(Volume::Decibels(Volume::MIN_DECIBELS));
-				tweenable.set(Volume::Decibels(0.0), tween);
-				tweenable
-			} else {
-				Tweener::new(Volume::Decibels(0.0))
-			},
+			volume_fade: create_volume_fade_tweener(settings),
 			shared: Arc::new(Shared {
 				state: AtomicU8::new(PlaybackState::Playing as u8),
 				position: AtomicU64::new(position.to_bits()),
@@ -157,16 +107,16 @@ impl StaticSound {
 	fn increment_position(&mut self) -> bool {
 		if let Some(LoopBehavior { start_position }) = self.data.settings.loop_behavior {
 			let start_position = (start_position * self.data.sample_rate as f64) as usize;
-			if self.current_sample_index >= self.data.frames.len() - 1 {
-				self.current_sample_index = start_position;
+			if self.current_frame_index >= self.data.frames.len() - 1 {
+				self.current_frame_index = start_position;
 			} else {
-				self.current_sample_index += 1;
+				self.current_frame_index += 1;
 			}
 		} else {
-			if self.current_sample_index >= self.data.frames.len() - 1 {
+			if self.current_frame_index >= self.data.frames.len() - 1 {
 				return true;
 			} else {
-				self.current_sample_index += 1;
+				self.current_frame_index += 1;
 			}
 		}
 		false
@@ -177,36 +127,30 @@ impl StaticSound {
 	fn decrement_position(&mut self) -> bool {
 		if let Some(LoopBehavior { start_position }) = self.data.settings.loop_behavior {
 			let start_position = (start_position * self.data.sample_rate as f64) as usize;
-			if self.current_sample_index <= start_position {
-				self.current_sample_index = self.data.frames.len() - 1;
+			if self.current_frame_index <= start_position {
+				self.current_frame_index = self.data.frames.len() - 1;
 			} else {
-				self.current_sample_index -= 1;
+				self.current_frame_index -= 1;
 			}
 		} else {
-			if self.current_sample_index == 0 {
+			if self.current_frame_index == 0 {
 				return true;
 			} else {
-				self.current_sample_index -= 1;
+				self.current_frame_index -= 1;
 			}
 		}
 		false
 	}
 
-	/// Updates the playback position and pushes a new sample to the resampler.
+	/// Updates the current frame index by 1 and pushes a new sample to the resampler.
 	fn update_position(&mut self) {
-		let playback_rate = self.playback_rate();
 		if matches!(self.state, PlaybackState::Paused | PlaybackState::Stopped) {
 			self.resampler
-				.push_frame(Frame::ZERO, self.current_sample_index);
+				.push_frame(Frame::ZERO, self.current_frame_index);
 			return;
 		}
-		let out = self.data.frames[self.current_sample_index];
-		let out = (out
-			* self.volume_fade.value().as_amplitude() as f32
-			* self.volume.value().as_amplitude() as f32)
-			.panned(self.panning.value() as f32);
-		self.resampler.push_frame(out, self.current_sample_index);
-		let reached_end_of_sound = if playback_rate.is_sign_negative() {
+		self.push_frame_to_resampler();
+		let reached_end_of_sound = if self.playback_rate().is_sign_negative() {
 			self.decrement_position()
 		} else {
 			self.increment_position()
@@ -217,16 +161,16 @@ impl StaticSound {
 	}
 
 	fn seek_to_index(&mut self, index: usize) {
-		self.current_sample_index = index;
+		self.current_frame_index = index;
 		// if the seek index is past the end of the sound and the sound is
 		// looping, wrap the seek point back into the sound
 		if let Some(LoopBehavior { start_position }) = self.data.settings.loop_behavior {
 			let start_position = (start_position * self.data.sample_rate as f64) as usize;
-			while self.current_sample_index >= self.data.frames.len() {
-				self.current_sample_index -= self.data.frames.len() - start_position;
+			while self.current_frame_index >= self.data.frames.len() {
+				self.current_frame_index -= self.data.frames.len() - start_position;
 			}
 		// otherwise, stop the sound
-		} else if self.current_sample_index >= self.data.frames.len() {
+		} else if self.current_frame_index >= self.data.frames.len() {
 			self.set_state(PlaybackState::Stopped);
 		}
 		// if the sound is playing, push a frame to the resample buffer
@@ -234,12 +178,28 @@ impl StaticSound {
 		if matches!(self.state, PlaybackState::Paused | PlaybackState::Stopped) {
 			return;
 		}
-		let out = self.data.frames[self.current_sample_index];
+		self.push_frame_to_resampler();
+	}
+
+	fn push_frame_to_resampler(&mut self) {
+		let out = self.data.frames[self.current_frame_index];
 		let out = (out
 			* self.volume_fade.value().as_amplitude() as f32
 			* self.volume.value().as_amplitude() as f32)
 			.panned(self.panning.value() as f32);
-		self.resampler.push_frame(out, self.current_sample_index);
+		self.resampler.push_frame(out, self.current_frame_index);
+	}
+
+	fn seek_by(&mut self, amount: f64) {
+		let current_position = self.current_frame_index as f64 / self.data.sample_rate as f64;
+		let position = current_position + amount;
+		let index = (position * self.data.sample_rate as f64) as usize;
+		self.seek_to_index(index);
+	}
+
+	fn seek_to(&mut self, position: f64) {
+		let index = (position * self.data.sample_rate as f64) as usize;
+		self.seek_to_index(index);
 	}
 }
 
@@ -265,15 +225,10 @@ impl Sound for StaticSound {
 				Command::Resume(tween) => self.resume(tween),
 				Command::Stop(tween) => self.stop(tween),
 				Command::SeekBy(amount) => {
-					let current_position =
-						self.current_sample_index as f64 / self.data.sample_rate as f64;
-					let position = current_position + amount;
-					let index = (position * self.data.sample_rate as f64) as usize;
-					self.seek_to_index(index);
+					self.seek_by(amount);
 				}
 				Command::SeekTo(position) => {
-					let index = (position * self.data.sample_rate as f64) as usize;
-					self.seek_to_index(index);
+					self.seek_to(position);
 				}
 			}
 		}
@@ -316,5 +271,63 @@ impl Sound for StaticSound {
 
 	fn finished(&self) -> bool {
 		self.state == PlaybackState::Stopped && self.resampler.outputting_silence()
+	}
+}
+
+/// The playback state of a sound.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PlaybackState {
+	/// The sound is playing normally.
+	Playing,
+	/// The sound is fading out, and when the fade-out
+	/// is finished, playback will pause.
+	Pausing,
+	/// Playback is paused.
+	Paused,
+	/// The sound is fading out, and when the fade-out
+	/// is finished, playback will stop.
+	Stopping,
+	/// The sound has stopped and can no longer be resumed.
+	Stopped,
+}
+
+pub(super) struct Shared {
+	state: AtomicU8,
+	position: AtomicU64,
+}
+
+impl Shared {
+	pub fn state(&self) -> PlaybackState {
+		match self.state.load(Ordering::SeqCst) {
+			0 => PlaybackState::Playing,
+			1 => PlaybackState::Pausing,
+			2 => PlaybackState::Paused,
+			3 => PlaybackState::Stopping,
+			4 => PlaybackState::Stopped,
+			_ => panic!("Invalid playback state"),
+		}
+	}
+
+	pub fn position(&self) -> f64 {
+		f64::from_bits(self.position.load(Ordering::SeqCst))
+	}
+}
+
+fn starting_frame_index(settings: StaticSoundSettings, data: &StaticSoundData) -> usize {
+	if settings.reverse {
+		let position_seconds = data.duration().as_secs_f64() - settings.start_position;
+		(position_seconds * data.sample_rate as f64) as usize - 1
+	} else {
+		(settings.start_position * data.sample_rate as f64) as usize
+	}
+}
+
+fn create_volume_fade_tweener(settings: StaticSoundSettings) -> Tweener<Volume> {
+	if let Some(tween) = settings.fade_in_tween {
+		let mut tweenable = Tweener::new(Volume::Decibels(Volume::MIN_DECIBELS));
+		tweenable.set(Volume::Decibels(0.0), tween);
+		tweenable
+	} else {
+		Tweener::new(Volume::Decibels(0.0))
 	}
 }
