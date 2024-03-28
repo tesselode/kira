274231cc47540d1ba2c02f424565f9986bb2e295
Andrew Minnich <aminnich3@gmail.com>
2024-03-28 04:30:11 -0400

    single command writers/readers

---
+++ crates/kira/src/command.rs
@@ -0,0 +1,59 @@
+use triple_buffer::{triple_buffer, Input, Output};
+
+use crate::tween::{Tween, Value};
+
+pub struct CommandWriter<T: Send + Clone>(Input<Option<T>>);
+
+impl<T: Send + Clone> CommandWriter<T> {
+	pub fn write(&mut self, command: T) {
+		self.0.write(Some(command))
+	}
+}
+
+pub struct CommandReader<T: Send + Clone>(Output<Option<T>>);
+
+impl<T: Send + Clone> CommandReader<T> {
+	pub fn read(&mut self) -> Option<&T> {
+		if self.0.update() {
+			self.0.output_buffer().as_ref()
+		} else {
+			None
+		}
+	}
+}
+
+pub fn command_writer_and_reader<T: Send + Clone>() -> (CommandWriter<T>, CommandReader<T>) {
+	let (input, output) = triple_buffer(&None);
+	(CommandWriter(input), CommandReader(output))
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ValueChangeCommand<T> {
+	pub target: Value<T>,
+	pub tween: Tween,
+}
+
+macro_rules! command_writers_and_readers {
+	($($field_name:ident: $type:ty),*$(,)?) => {
+		pub(crate) struct CommandWriters {
+			$($field_name: $crate::command::CommandWriter<$type>),*
+		}
+
+		pub(crate) struct CommandReaders {
+			$($field_name: $crate::command::CommandReader<$type>),*
+		}
+
+		pub(crate) fn command_writers_and_readers() -> (CommandWriters, CommandReaders) {
+			paste::paste! {
+				$(let ([<$field_name _writer>], [<$field_name _reader>]) = $crate::command::command_writer_and_reader();)*
+				let command_writers = CommandWriters {
+					$($field_name: [<$field_name _writer>]),*
+				};
+				let command_readers = CommandReaders {
+					$($field_name: [<$field_name _reader>]),*
+				};
+				(command_writers, command_readers)
+			}
+		}
+	};
+}
