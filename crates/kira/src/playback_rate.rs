9a873233d32a97fd258e24db1028176be2fdb620
Andrew Minnich <aminnich3@gmail.com>
2024-09-08 20:01:46 -0400

    use serde(transparent) for unit types

--- crates/kira/src/playback_rate.rs
+++ crates/kira/src/playback_rate.rs
@@ -5,6 +5,8 @@ use std::ops::{
 use crate::tween::{Tweenable, Value};
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "serde", serde(transparent))]
 pub struct PlaybackRate(pub f64);
 
 impl Default for PlaybackRate {
